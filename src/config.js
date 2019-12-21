9d4e2d00eef03a3637a2dbc3179c0af9f8e6df89
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-12-21 09:40:41 -0500

    added npm

---
+++ src/config.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+module.exports = {
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+    TOKEN_KEY: 'cgc-client-auth-token',
+    JWT_EXPIRY: process.env.JWY_EXPIRY || '20s',
+};
